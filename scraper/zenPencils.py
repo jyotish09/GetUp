e8c4b870cfde6e30d22edee81e61c8af80a67600
jyotish09 <dontsentmemails@yahoo.com>
2018-02-16 22:38:28 +0530

    Pyrebase

--- scraper/zenPencils.py
+++ scraper/zenPencils.py
@@ -1,6 +1,7 @@
 import requests, urllib, re, os, json, pyrebase
 from bs4 import BeautifulSoup as BS
-
+import txt
+print(txt.user)
 session = requests.session()
 pageURL = 'https://zenpencils.com/'
 page = session.post(pageURL)
