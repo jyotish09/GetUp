040452efc4b116c4799542406e66e487beb63a8c
jypaul <dontsentmemails@yahoo.com>
2018-02-20 10:06:04 +0530

    JSON Try

--- scraper/hesMotivation.py
+++ scraper/hesMotivation.py
@@ -8,7 +8,7 @@ url = 'https://www.googleapis.com/youtube/v3/search'
 
 params = dict(
     order='date',
-    # pageToken='CPoBEAA',
+    pageToken='CMgBEAA',
     part='snippet',
     channelId='UC3gWv-0A3qEeFBJESlsJa0g',
     maxResults=50,
@@ -18,5 +18,16 @@ print('params')
 print(params)
 resp = requests.get(url=url, params=params)
 data = json.loads(resp.text)
-print("\n data \n")
-print(get_pretty_print(data))
+# print("\n data \n")
+# print(get_pretty_print(data))
+if 'nextPageToken' in data:
+    print('\n data["nextPageToken"] \n')
+    print(data["nextPageToken"])
+if 'prevPageToken' in data:
+    print('\n data["prevPageToken"] \n')
+    print(data["prevPageToken"])
+print("\n Looping through items ... \n")
+
+for i in data["items"]:
+    print("\n")
+    print(get_pretty_print(i))
