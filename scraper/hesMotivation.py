c854004bda6316a7f83502d0aed6b58169fd92ef
jypaul <dontsentmemails@yahoo.com>
2018-02-22 08:41:55 +0530

    Time Duration Check

--- scraper/hesMotivation.py
+++ scraper/hesMotivation.py
@@ -1,14 +1,14 @@
-import pyrebase, json, requests
+import pyrebase, json, requests, isodate
 import txt
 
 def pretty_print_json(json_object):
     return json.dumps(json_object, sort_keys=True, indent=4, separators=(',', ': '))
 
-def youtube_list_videos():
+def youtube_list_videos(pageToken):
     url = 'https://www.googleapis.com/youtube/v3/search'
     params = dict(
         order='date',
-        pageToken='CMgBEAA',
+        pageToken=pageToken,
         part='snippet',
         channelId='UC3gWv-0A3qEeFBJESlsJa0g',
         maxResults=50,
@@ -19,7 +19,17 @@ def youtube_list_videos():
     resp = requests.get(url=url, params=params)
     return json.loads(resp.text)
 
-data = youtube_list_videos()
+def videoDetails(id):
+    urlVid = 'https://www.googleapis.com/youtube/v3/videos'
+    paramsVid= dict(
+    id=id,
+    part='contentDetails',
+    key=txt.youtubeAPIKey
+    )
+    respVid = requests.get(url=urlVid, params=paramsVid)
+    return json.loads(respVid.text)
+
+data = youtube_list_videos('CMgBEAA')
 # print("\n data \n")
 # print(pretty_print_json(data))
 if 'nextPageToken' in data:
@@ -34,21 +44,17 @@ for i in data["items"]:
     if 'videoId' in i["id"]:
         print("\n")
         print(i["snippet"]["title"])
-        paramsVid= dict(
-        id=i["id"]["videoId"],
-        part='contentDetails',
-        key=txt.youtubeAPIKey
-        )
-        print('paramsVid')
-        print(pretty_print_json(paramsVid["id"]))
-        urlVid = 'https://www.googleapis.com/youtube/v3/videos'
-        respVid = requests.get(url=urlVid, params=paramsVid)
-        dataVid = json.loads(respVid.text)
+        print(pretty_print_json(i["id"]["videoId"]))
+
+        dataVid = videoDetails(i["id"]["videoId"])
         # print(pretty_print_json(dataVid))
         print(pretty_print_json(dataVid["items"][0]["contentDetails"]["duration"]))
-        if  dataVid["items"][0]["contentDetails"]["duration"] <= "PT9M59S" :
+        dur=isodate.parse_duration(dataVid["items"][0]["contentDetails"]["duration"]).total_seconds()
+        print(dur)
+
+        if  dur <= 599 :
             print("<=PT9M59S")
-        if  "PT12M30S" > dataVid["items"][0]["contentDetails"]["duration"] > "PT10M00S" :
+        if  750 > dur > 599 :
             print("PT12M30S  -  PT10M00S")
-        else : # dataVid["items"][0]["contentDetails"]["duration"] > "PT12M30S":
+        if  750 <= dur :
             print("Too Long")
