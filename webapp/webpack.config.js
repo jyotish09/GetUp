8cc5bade29ea09b796515ea3f5e9c772a36c8f85
jyotish09 <dontsentmemails@yahoo.com>
2018-02-25 16:26:47 +0530

    React Set Up

---
+++ webapp/webpack.config.js
@@ -0,0 +1,63 @@
+/*
+    ./webpack.config.js
+*/
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: './client/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})
+
+module.exports = {
+    entry: './client/index.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        loaders: [{
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.jsx$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            // Used for Bootstrap Less Source Files
+            {
+                test: /\.less$/,
+                use: [{
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "less-loader"
+                    }
+                ]
+            },
+            // Used for Bootstrap Less Source Files
+            {
+                test: /\.css$/,
+                use: [{
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    }
+                ]
+            },
+            // Used for Bootstrap Glyphicon Fonts
+            {
+                test: /\.(woff2|woff|ttf|svg|eot)$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+    plugins: [HtmlWebpackPluginConfig]
+}
