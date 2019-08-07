7f9a6287f865301018f7191d8b09f9bfe8607f3c
jypaul <jypaul@cisco.com>
2019-08-07 05:47:10 +0530

    Hello!

--- webapp/App.js
+++ webapp/App.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Am I alive ?</Text>
-    </View>
-  );
+export default class DisplayAnImage extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <ImageBackground source={{uri: "http://placekitten.com/408/287"}} style={styles.image}>
+          <Text style={styles.instructions}>
+            Hello !
+            </Text>
+        </ImageBackground>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'stretch',
+    backgroundColor: '#F5FCFF',
+  },
+  image: {
+    height: 500,
+    justifyContent: "space-around",    //  <-- you can use "center", "flex-start",
+    resizeMode: "contain",             //      "flex-end" or "space-between" here
+  },
+  instructions: {
+    textAlign: 'center',
+    color: 'white',
+    backgroundColor: "transparent",
+    fontSize: 25,
   },
 });
