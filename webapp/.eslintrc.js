ab897cfc170264231a6872b1320f46db82958ccc
jypaul <jypaul@cisco.com>
2019-08-12 12:32:47 +0530

    ESLint Addition

---
+++ webapp/.eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+    'eslint:recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    fetch: false
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+		'jsx-a11y',
+		'import',
+		'compat'
+  ],
+  rules: {
+    'no-use-before-define': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'comma-dangle': 'off',
+    'no-console': 'off',
+		'indent': ['error', 4, { 'SwitchCase': 1 }],
+		'semi': ['error', 'always'], //Always use semicolons at the end of the statement
+		'jsx-quotes': ['error', 'prefer-double'],
+		'one-var': 'off',
+		'react/jsx-indent': ['error', 4],
+		'react/jsx-indent-props': ['error', 4],
+		'max-len': [2, 150, 2, {'ignoreComments': true}], //Max 150 characters allowed in a line,
+		'arrow-body-style': ['error', 'as-needed', { 'requireReturnForObjectLiteral': true }],
+		'compat/compat': 'error',
+    'no-undef': 'error',
+    'react/prefer-stateless-function': 0,
+    'func-names': 0,
+    'import/no-unresolved': 0
+  },
+  parser: 'babel-eslint',
+};
--- webapp/App.js
+++ webapp/App.js
@@ -1,36 +1,41 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import {
+    StyleSheet, Text, View, ImageBackground,
+} from 'react-native';
 
 export default class DisplayAnImage extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <ImageBackground source={{uri: "http://placekitten.com/408/287"}} style={styles.image}>
-          <Text style={styles.instructions}>
-            Hello !
-            </Text>
-        </ImageBackground>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <ImageBackground
+                    source={{ uri: 'https://w.wallhaven.cc/full/xl/wallhaven-xll76z.jpg' }}
+                    style={styles.image}
+                >
+                    <Text style={styles.instructions}>
+                        Hello !
+                    </Text>
+                </ImageBackground>
+            </View>
+        );
+    }
 }
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'stretch',
-    backgroundColor: '#F5FCFF',
-  },
-  image: {
-    height: 500,
-    justifyContent: "space-around",    //  <-- you can use "center", "flex-start",
-    resizeMode: "contain",             //      "flex-end" or "space-between" here
-  },
-  instructions: {
-    textAlign: 'center',
-    color: 'white',
-    backgroundColor: "transparent",
-    fontSize: 25,
-  },
+let styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        backgroundColor: '#F5FCFF',
+    },
+    image: {
+        height: 500,
+        justifyContent: 'space-around',
+        resizeMode: 'contain',
+    },
+    instructions: {
+        textAlign: 'center',
+        color: 'white',
+        backgroundColor: 'transparent',
+        fontSize: 25,
+    },
 });
