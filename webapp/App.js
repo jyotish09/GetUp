7d0214d7fd0e49a27952c44eb68bf6ccca720d3e
jyotish09 <dontsentmemails@yahoo.com>
2019-08-12 18:01:03 +0530

    List Tuts

--- webapp/App.js
+++ webapp/App.js
@@ -1,13 +1,54 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import {
+    View, StyleSheet, FlatList, Text,
+    SectionList
+} from 'react-native';
 
-export default class FlexDirectionBasics extends Component {
+export default class ListItemsViewTest extends Component {
     render() {
         return (
-            <View style={styles.alignItems}>
-                <View style={styles.powderBlue} />
-                <View style={styles.skyBlue} />
-                <View style={styles.steelBlue} />
+            <SectionListBasics />
+        );
+    }
+}
+
+class FlatListTuts extends Component {
+    render() {
+        return (
+            <View style={styles.listCSS}>
+                <FlatList
+                    data={[
+                        { key: 'Devin' },
+                        { key: 'Dustin' },
+                        { key: 'Dominic' },
+                        { key: 'Jackson' },
+                        { key: 'James' },
+                        { key: 'Joel' },
+                        { key: 'John' },
+                        { key: 'Jillian' },
+                        { key: 'Jimmy' },
+                        { key: 'Julie' },
+                    ]}
+                    renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
+                />
+            </View>
+        );
+    }
+}
+
+class SectionListBasics extends Component {
+    render() {
+        return (
+            <View style={styles.listCSS}>
+                <SectionList
+                    sections={[
+                        { title: 'D', data: ['Devin', 'Dustin', 'Dominic'] },
+                        { title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie'] }
+                    ]}
+                    renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
+                    renderSectionHeader={({ section }) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+                    keyExtractor={(item, index) => index}
+                />
             </View>
         );
     }
@@ -40,5 +81,27 @@ let styles = StyleSheet.create({
     },
     steelBlue: {
         width: 75, height: 75, backgroundColor: 'steelblue'
+    },
+    listCSS: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        // alignItems: 'center',
+        marginTop: '10%',
+        width: '100%'
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+    },
+    sectionHeader: {
+        paddingTop: 2,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingBottom: 2,
+        fontSize: 14,
+        fontWeight: 'bold',
+        backgroundColor: 'rgba(247,247,247,1.0)',
     }
 });
