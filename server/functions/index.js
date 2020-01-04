032bf06abd49badb952ff2a9f707a7d9689da658
jyotish09 <dontsentmemails@yahoo.com>
2020-01-04 19:07:58 +0530

    A new random article for everyday
    And storing it in the database

--- server/functions/index.js
+++ server/functions/index.js
@@ -12,15 +12,28 @@ exports.alertTimeCreate = functions.database.ref('/userDetails/expoDeviceIDs/')
         console.log('snapshot.val() : ', snapshot.val());
         console.log('context : ', context);
         return snapshot.val();
-    });
+});
+
+/**
+ * Everyday a new link to a comic or a video etc should be kept at
+ * `todaysArticle` node in FDB
+ * @param {obj} data 
+ */
+let createTodaysArticle = function (data) {
+    const ref = db.ref('todaysArticle');
+    ref.update(data);
+};
 
 /*
  * For getting a random article from the collection for the day
+ * [Link for that](https://us-central1-getupyoufool160218.cloudfunctions.net/getTodaysArticle)
 */
 exports.getTodaysArticle = functions.https.onRequest((req, res) => {
     const randomNode = utils.randomCollectionNode();
     const ref = db.ref(randomNode);
     ref.once("value", (snapshot) => {
-        res.send(utils.randomObject(snapshot.val(), randomNode));
+        const todaysArticle = utils.randomObject(snapshot.val(), randomNode);
+        createTodaysArticle(todaysArticle);
+        res.send(todaysArticle);
     });
 });
--- server/functions/utils.js
+++ server/functions/utils.js
@@ -13,5 +13,19 @@ exports.randomObject = (obj, randomNode) => {
     let keys = Object.keys(obj)
     /* https://jsperf.com/random-object-property-selection */
     let randomObj = obj[keys[ keys.length * Math.random() << 0]]
-    return Object.assign({}, {node: randomNode}, randomObj);
+    switch (randomNode) {
+        case "zenpencils": response = ({
+            linkName: randomObj.key,
+            link: randomObj.value,
+            nodeName: randomNode
+        });
+        break;
+        case "absoluteMotivation":
+        case "hesMotivation": response = ({
+            linkName: randomObj.clipName,
+            link: `https://youtu.be/${randomObj.id}`,
+            nodeName: randomNode
+        });
+    }
+    return response;
 }
