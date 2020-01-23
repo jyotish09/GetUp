9f502d9e613b574465c366762996ccaff192c2e8
jyotish09 <dontsentmemails@yahoo.com>
2020-01-23 15:57:31 +0530

    Clean Ups

--- webapp/assets/messages.js
+++ webapp/assets/messages.js
@@ -3,7 +3,7 @@
 */
 
 export const message_pack = {
-    handlePlaceholder: "Your handle",
-    alertTime: "Alert Time",
-    submitButtonMessage: "Notify me @ this time"
+    handlePlaceholder: 'Your handle',
+    alertTime: 'Alert Time',
+    submitButtonMessage: 'Notify me @ this time'
 };
